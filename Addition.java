9efc98ad0ff4380576cc9d1dc552712b545f7a30
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-07-04 08:26:46 +0700

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    int sum(int x, int y ){
+        return x + y ;
+    }
+    double sum(double x, double y){
+        return x + y ;
+    }
+    String sum(String s1, String s2){
+        return s1.concat(s2);
+    }
+}
